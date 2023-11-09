a30a02155e407f77842fe2356ee51f37865086f3
glzawalski <gabrielzawalski@gmail.com>
2023-11-09 18:51:04 -0300

    Initial Commit

---
+++ FetchAsync/FetchAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAsyncApp.swift
+//  FetchAsync
+//
+//  Created by Gabriel Zawalski on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
